2507c0ebfdb818f31082aed2c2d3392d914ae2b7
guiottoo <guilhermeotto22@gmail.com>
2025-06-30 00:19:03 -0300

    styles: responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,7 @@ function App() {
           <img src="/assets/3d-render-hand-put-golden-coin-into-piggy-bank.png" width='350px'/>
           <CDIForm/>
         </div>
-
-        <div className="container flex-column d-flex justify-content-center align-items-center form_container">
-          <h1>CDIPlanner</h1>
-          <img src="/assets/3d-render-hand-put-golden-coin-into-piggy-bank.png" width='350px'/>
-          <CDIForm/>
-        </div>
-
-        <div className="container flex-column d-flex justify-content-center align-items-center form_container">
-          <h1>CDIPlanner</h1>
-          <img src="/assets/3d-render-hand-put-golden-coin-into-piggy-bank.png" width='350px'/>
-          <CDIForm/>
-        </div>
       </div>
-
-      
   );
 }
 
